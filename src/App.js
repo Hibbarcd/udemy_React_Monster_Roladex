ade8ca905fff11ccc90ef4ba037389b59d5d6977
Black Jack <hibbarcd@gmail.com>
2019-07-11 10:49:37 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      monsters: [
+        {
+        name: 'Frankenstein',
+        id: 'ascribe12'
+      },
+      {
+        name: 'Dracula',
+        id: 'describe245'
+      },
+      {
+        name: 'Zombie',
+        id: 'rescribe432'
+      }
+      ]
+    }
+  }
+render() {
+  return (
+    <div className="App">
+    {
+      this.state.monsters.map(monster => <h1 key={ monster.id } > { monster.name }</h1>)
+    }
+    </div>
+)
+}
+}
+export default App;
