99a59635e57be54e1380d61c29da0dde3fff7a0a
Black Jack <hibbarcd@gmail.com>
2019-07-15 11:22:15 -0500

    Updates

---
+++ precache-manifest.de98ff19d1e0c2cc51873b78f3c2fa4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d147ef0b45394533d877afff96779de",
+    "url": "/udemy_React_Monster_Rolodex/index.html"
+  },
+  {
+    "revision": "d4cbbdd1654cbeca7380",
+    "url": "/udemy_React_Monster_Rolodex/static/css/main.3e5a0266.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/udemy_React_Monster_Rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "d4cbbdd1654cbeca7380",
+    "url": "/udemy_React_Monster_Rolodex/static/js/main.814c7b73.chunk.js"
+  },
+  {
+    "revision": "662b82dbf6c1550a746c",
+    "url": "/udemy_React_Monster_Rolodex/static/js/runtime~main.705df575.js"
+  }
+]);
