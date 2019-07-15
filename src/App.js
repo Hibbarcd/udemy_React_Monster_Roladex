997c3b1e438e639012ef4938cbf23473d0bb19a3
Black Jack <hibbarcd@gmail.com>
2019-07-14 22:24:12 -0500

    working searchbox component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import {CardList} from './components/card-list/card-list.component'
-
+import {SearchBox} from './components/search-box/search-box.component'
 
 class App extends Component {
   constructor() {
@@ -29,12 +29,11 @@ render() {
 
   return (
     <div className="App">
-      <input 
-        type='search' 
-        placeholder="search monsters"
-        // code below logs value of input from keyboard and console.logs it, but it has to be set as the 2nd parameter of the setState function because it is async.
-        onChange={e => this.setState({ searchfield: e.target.value }, () => console.log(this.state)) }/>
 
+    <SearchBox 
+            placeholder="search monsters"
+            handleChange={e => this.setState({ searchfield: e.target.value }, () => console.log(this.state)) }
+    />
     <CardList monsters={filteredMonsters} />
 
     </div>
